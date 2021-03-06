a3be471e8a2b48992c0fdf4dedaaa6d5b63099a7
“ryouta.yamada” <ryamada.bas316@gmail.com>
2021-03-06 04:15:11 +0000

    follow, unfollow

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -3,9 +3,12 @@
         {{-- トップページへのリンク --}}
         <a class="navbar-brand" href="/">Microposts</a>
 
+        /*
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
             <span class="navbar-toggler-icon"></span>
         </button>
+        */
+        
 
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
